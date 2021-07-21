2be4a39bc05e6044243a65322c2b46cf78f4592d
palabbe <pauline.labbe7@gmail.com>
2021-07-21 16:32:05 +0200

    get et put ok

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,16 @@
-import React from "react";
-import {
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import Theme, {
   Sizes,
   Title,
   BorderBottom,
   Text,
   Container,
+  Button,
 } from "../assets/styles/Theme";
-import styled, { keyframes } from "styled-components";
 import juliette from "../assets/img/juliette.jpeg";
+import styled, { keyframes } from "styled-components";
 import { fadeInLeft } from "react-animations";
 
 const fadeInLeftAnimation = keyframes`${fadeInLeft}`;
@@ -29,6 +32,21 @@ const TextContainer = styled(Container)`
   }
 `;
 
+const Input = styled.input`
+  padding: 0.5rem;
+  width: 100%;
+  height: 9rem;
+  border: none;
+  box-shadow: 0px 3px 5px 2px rgba(0, 0, 0, 0.3);
+  &:hover {
+    border: 1px solid ${Theme.colorBeige};
+  }
+  &:focus {
+    outline: none;
+    border: 1px solid ${Theme.colorOchre};
+  }
+`;
+
 const Picture = styled.img`
   width: 30rem;
   margin-top: 5rem;
@@ -38,17 +56,49 @@ const Picture = styled.img`
 `;
 
 export default function About() {
+  const [aboutInfos, setAboutInfos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [text, setText] = useState("");
+  const [newChange, setNewChange] = useState(true);
+
+  useEffect(() => {
+    const getAboutData = async () => {
+      try {
+        const aboutData = await axios.get("http://localhost:4040/about");
+        setAboutInfos(aboutData.data[0]);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getAboutData();
+  }, [loading]);
+
+  const updateText = async () => {
+    try {
+      await axios.put("http://localhost:4040/about", {
+        text,
+      });
+      setNewChange(!newChange);
+      toast.success("Votre modification a été prise en compte");
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log(error);
+      toast.error(`${error.message}`);
+    } finally {
+      setText();
+    }
+  };
+
   return (
     <ComponentContainer flex aiCenter jcCenter>
       <TextContainer>
         <Title>ABOUT ME</Title>
         <BorderBottom />
-        <Text>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo,
-          numquam. Voluptate nam nostrum dolorum, ipsum consequatur eveniet ab
-          qui beatae vitae obcaecati mollitia, quo magni odio recusandae ea amet
-          tempora!
-        </Text>
+        <Text>{aboutInfos.text}</Text>
+        <Input value={text} onChange={(e) => setText(e.target.value)} />
+        <Button onClick={updateText}>Modifier la description</Button>
       </TextContainer>
       <Picture src={juliette} />
     </ComponentContainer>
